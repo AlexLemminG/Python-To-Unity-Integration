39a93900866e1b88fe017d7ef4f0cf4caa6c46b2
Alexander <psp_4ever@mail.ru>
2018-03-05 05:22:59 +0300

    External python behaviour

---
+++ Assets/ExternalPythonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalPythonBehaviour : PythonBehaviour {
+	public string filePath = "testPython.py";
+	protected override void Awake(){
+		script = PythonScript.CreateFromFile (filePath);
+		base.Awake ();
+	}
+}
